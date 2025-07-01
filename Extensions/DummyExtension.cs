10815cad84e3e9c3e352f735d01d7e6ca9a3a30f
SlejmUr <slejmur@protonmail.com>
2025-07-01 19:52:56 +0200

    Add dmmyExtension

---
+++ Extensions/DummyExtension.cs
@@ -0,0 +1,87 @@
+﻿using InventorySystem.Items.Autosync;
+using PlayerRoles.Subroutines;
+
+namespace LabApiExtensions.Extensions;
+
+public static class DummyExtension
+{
+    public static bool TryRunItemAction(AutosyncItem item, ActionName actionName, bool isClick)
+    {
+        if (item == null)
+            return false;
+        if (!item.IsEmulatedDummy)
+            return false;
+        item.DummyEmulator.AddEntry(actionName, isClick);
+        return true;
+    }
+
+    public static bool TryStopItemAction(AutosyncItem item, ActionName actionName)
+    {
+        if (item == null)
+            return false;
+        if (!item.IsEmulatedDummy)
+            return false;
+        item.DummyEmulator.RemoveEntry(actionName);
+        return true;
+    }
+
+    public static bool TryRunItemAction<T>(T item, ActionName actionName, bool isClick) where T : AutosyncItem
+    {
+        if (item == null)
+            return false;
+        if (!item.IsEmulatedDummy)
+            return false;
+        item.DummyEmulator.AddEntry(actionName, isClick);
+        return true;
+    }
+
+    public static bool TryStopItemAction<T>(T item, ActionName actionName) where T : AutosyncItem
+    {
+        if (item == null)
+            return false;
+        if (!item.IsEmulatedDummy)
+            return false;
+        item.DummyEmulator.RemoveEntry(actionName);
+        return true;
+    }
+
+    public static bool TryRunRoleAction(SubroutineBase subroutine, ActionName actionName, bool isClick)
+    {
+        if (subroutine == null)
+            return false;
+        if (!subroutine.Role.IsEmulatedDummy)
+            return false;
+        subroutine.DummyEmulator.AddEntry(actionName, isClick);
+        return true;
+    }
+
+    public static bool TryStopRoleAction(SubroutineBase subroutine, ActionName actionName)
+    {
+        if (subroutine == null)
+            return false;
+        if (!subroutine.Role.IsEmulatedDummy)
+            return false;
+        subroutine.DummyEmulator.RemoveEntry(actionName);
+        return true;
+    }
+
+    public static bool TryRunRoleAction<T>(T subroutine, ActionName actionName, bool isClick) where T : SubroutineBase
+    {
+        if (subroutine == null)
+            return false;
+        if (!subroutine.Role.IsEmulatedDummy)
+            return false;
+        subroutine.DummyEmulator.AddEntry(actionName, isClick);
+        return true;
+    }
+
+    public static bool TryStopRoleAction<T>(T subroutine, ActionName actionName) where T : SubroutineBase
+    {
+        if (subroutine == null)
+            return false;
+        if (!subroutine.Role.IsEmulatedDummy)
+            return false;
+        subroutine.DummyEmulator.RemoveEntry(actionName);
+        return true;
+    }
+}
