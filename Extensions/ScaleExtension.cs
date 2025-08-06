241a3e6721f2e054b6e158daa7974a0bd717b333
SlejmUr <slejmur@protonmail.com>
2025-08-06 10:54:43 +0200

    fix tolist

--- Extensions/ScaleExtension.cs
+++ Extensions/ScaleExtension.cs
@@ -14,7 +14,7 @@ public static class ScaleHelper
 
         if (IsFake)
         {
-            PlayerFakeExtension.SetFakeScale(player, Player.ReadyList, value);
+            PlayerFakeExtension.SetFakeScale(player, Player.ReadyList.ToList(), value);
             return;
         }
 
