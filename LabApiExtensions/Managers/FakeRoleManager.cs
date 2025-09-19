c763b5fc76d11138eec7d4543ee5e465448c024d
SlejmUr <slejmur@protonmail.com>
2025-09-19 10:25:01 +0200

    removed unneccessary Event subscirbe removed writer sets (tested any worked with dummies)

--- LabApiExtensions/Managers/FakeRoleManager.cs
+++ LabApiExtensions/Managers/FakeRoleManager.cs
@@ -1,15 +1,8 @@
-﻿using LabApi.Features.Extensions;
-using LabApi.Events.Arguments.PlayerEvents;
+﻿using LabApi.Events.Arguments.PlayerEvents;
 using LabApi.Events.Handlers;
 using Mirror;
 using PlayerRoles;
-using PlayerRoles.FirstPersonControl;
 using PlayerRoles.FirstPersonControl.NetworkMessages;
-using PlayerRoles.PlayableScps.Scp049.Zombies;
-using PlayerRoles.PlayableScps.Scp1507;
-using PlayerStatsSystem;
-using RelativePositioning;
-using UnityEngine;
 
 namespace LabApiExtensions.Managers;
 
@@ -20,7 +13,6 @@ public static class FakeRoleManager
     static FakeRoleManager()
     {
         PlayerEvents.ChangingRole += PlayerEvents_ChangingRole;
-        PlayerEvents.Left += PlayerEvents_Left;
         FpcServerPositionDistributor.RoleSyncEvent += FpcServerPositionDistributor_RoleSyncEvent;
     }
 
@@ -28,11 +20,6 @@ public static class FakeRoleManager
     {
         if (ev.IsAllowed)
             RemoveFakeRole(ev.Player);
-    }
-
-    private static void PlayerEvents_Left(PlayerLeftEventArgs ev)
-    {
-        RemoveFakeRole(ev.Player);
     }
 
     public static void AddFakeRole(this Player player, RoleTypeId roleType)
@@ -48,50 +35,10 @@ public static class FakeRoleManager
         FakeRoles.Remove(player.ReferenceHub);
     }
 
-    private static RoleTypeId FpcServerPositionDistributor_RoleSyncEvent(ReferenceHub hub, ReferenceHub receiver, RoleTypeId roleType, Mirror.NetworkWriter writer)
+    private static RoleTypeId FpcServerPositionDistributor_RoleSyncEvent(ReferenceHub hub, ReferenceHub receiver, RoleTypeId roleType, NetworkWriter writer)
     {
         if (FakeRoles.TryGetValue(hub, out var value))
-        {
-            if (value == roleType)
-                return value;
-            WriteExtraForRole(hub, value, writer);
             return value;
-        }
         return roleType;
-    }
-
-    private static void WriteExtraForRole(ReferenceHub hub, RoleTypeId roleType, NetworkWriter writer)
-    {
-        PlayerRoleBase roleBase = roleType.GetRoleBase();
-
-        if (roleBase is HumanRole { UsesUnitNames: not false })
-        {
-            writer.WriteByte(0);
-        }
-
-        if (roleBase is ZombieRole)
-        {
-            writer.WriteUShort((ushort)Mathf.Clamp(Mathf.CeilToInt(hub.playerStats.GetModule<HealthStat>().MaxValue), 0, 65535));
-            writer.WriteBool(true);
-        }
-
-        if (roleBase is Scp1507Role)
-        {
-            writer.WriteByte((byte)hub.roleManager.CurrentRole.ServerSpawnReason);
-        }
-
-        FpcStandardRoleBase fpcStandardRoleBase = roleBase as FpcStandardRoleBase;
-        if (fpcStandardRoleBase != null)
-        {
-            if (hub.roleManager.CurrentRole is FpcStandardRoleBase fpcStandardRoleBase2)
-            {
-                fpcStandardRoleBase = fpcStandardRoleBase2;
-            }
-
-            ushort syncH = 0;
-            fpcStandardRoleBase?.FpcModule.MouseLook.GetSyncValues(0, out syncH, out var _);
-            writer.WriteRelativePosition(new(hub));
-            writer.WriteUShort(syncH);
-        }
     }
 }
